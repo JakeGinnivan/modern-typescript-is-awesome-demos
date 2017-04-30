19d6011e8dee9033860694dac4322172c66a1c58
Jake Ginnivan <jake@ginnivan.net>
2017-04-30 17:58:32 +0800

    Setup reducer

--- demos/1. Descriminated Unions.ts
+++ demos/1. Descriminated Unions.ts
@@ -1,17 +1,20 @@
-const LOAD_DATA = 'LOAD_DATA'
-type LOAD_DATA = {
+type LOAD_DATA = 'LOAD_DATA'
+const LOAD_DATA: LOAD_DATA = 'LOAD_DATA'
+type LOAD_DATA_ACTION = {
     type: LOAD_DATA
 }
 
-type Action = LOAD_DATA
+type Action = LOAD_DATA_ACTION
 
 type State = {
 
 }
 
-const reducer = (state: State, action: LOAD_DATA) => {
+const reducer = (state: State = {}, action: Action) => {
     switch (action.type) {
         default:
             state
     }
 }
+
+reducer({}, { type: LOAD_DATA })
